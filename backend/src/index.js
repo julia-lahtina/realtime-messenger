e73a77d03d946f4bc7f2b1c7a6134c5591455282
Julia Lakhtin <julia.lakhtin@nteam.de>
2025-07-29 11:40:40 +0200

    added routes backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
 
 const app = express();
 
-app.listen(5001, () => {
-  console.log("server is running on port 5001");
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on PORT: ${PORT}`);
+  connectDB();
 });
